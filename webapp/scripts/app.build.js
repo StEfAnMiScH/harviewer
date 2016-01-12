4d2c0baefd27f68b75956c6bd6ac2f401ff0b581
Tim Hemming <timhemming@gmail.com>
2016-01-12 21:52:52 +0000

    Removed syntax highlighting as it was causing performance issues.

--- webapp/scripts/app.build.js
+++ webapp/scripts/app.build.js
@@ -3,7 +3,7 @@
 
     baseUrl: "scripts/",
 
-    dir: "../../webapp-build",
+    dir: "../../target",
 
     // Comment out the optimize line if you want
     // the code minified by Closure Compiler using
